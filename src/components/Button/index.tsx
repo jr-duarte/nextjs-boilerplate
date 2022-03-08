89b4386ee085d00828a099a3c5aa81da5c392353
Junior Duarte <jr.duartee.93@gmail.com>
2022-03-08 13:02:42 -0300

    chore: ajusta botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,5 @@
 import * as S from './styles';
 
 export default function Button() {
-  return (
-    <>
-      <S.Button>Almost before we knew it, we had</S.Button>
-    </>
-  );
+  return <S.Button>Almost before we knew it, we had</S.Button>;
 }

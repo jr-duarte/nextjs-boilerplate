ca808c7a16a77abd705c2daaade3bb8a1a4b8d5b
Junior Duarte <junior.duarte@casion.com.br>
2022-10-04 21:39:22 -0300

    chore: ajustes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,10 @@ import Container from '@Components/core/Container';
 import GridCol from '@Components/core/Grid/Col';
 import GridRow from '@Components/core/Grid/Row';
 import ModalTeste from '@Components/ModalTeste';
+import { PATH } from '@Routes/paths';
 import TemplateDefault from '@Templates/Default';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   const [modalShow, setModalShow] = useState<boolean>(false);
@@ -28,6 +30,7 @@ const Home: NextPage = () => {
             </button>
             <ModalTeste isOpen={modalShow} onClose={handleModalClose} />
             <h1>Teste</h1>
+            <Link href={PATH.notloggedArea.login}>Login</Link>
           </GridCol>
         </GridRow>
       </Container>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,7 +4,9 @@ import Container from '@Components/core/Container';
 import GridCol from '@Components/core/Grid/Col';
 import GridRow from '@Components/core/Grid/Row';
 import { NextPageWithLayout } from '@Core/types/next';
+import { PATH } from '@Routes/paths';
 import TemplateDefault from '@Templates/Default';
+import Link from 'next/link';
 
 const Login: NextPageWithLayout = () => {
   return (
@@ -13,6 +15,7 @@ const Login: NextPageWithLayout = () => {
         <GridRow>
           <GridCol col={10}>
             <h1>Login</h1>
+            <Link href={PATH.notloggedArea.home}>Home</Link>
           </GridCol>
         </GridRow>
       </Container>

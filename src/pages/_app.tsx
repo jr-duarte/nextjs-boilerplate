99a4d1d93bf541665ac4f113636da5d63972661b
Junior Duarte <junior.duarte@casion.com.br>
2022-11-20 09:39:32 -0300

    chore: ajustes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { ReactTagManager } from 'react-gtm-ts';
 
 import { NextPageWithLayout } from '@Core/types/next';
@@ -11,20 +10,6 @@ type AppPropsWithLayout = AppProps & {
 ReactTagManager.init('GTM-NGV448C');
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  useEffect(() => {
-    window.addEventListener('load', (event) => {
-      console.log(event);
-    });
-
-    window.addEventListener('fecth', (event) => {
-      console.log(event);
-    });
-
-    window.addEventListener('fetch', function (event) {
-      console.log(event);
-    });
-  }, []);
-
   return <Component {...pageProps} />;
 }
 export default MyApp;
